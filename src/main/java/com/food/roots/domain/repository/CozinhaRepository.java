6897e083f5a09674a5174ec74e547c516f5f0211
João Geraldo <joaogeraldo40@gmail.com>
2022-12-20 15:29:37 -0300

    Outra forma de criar consulta dinâmica

--- src/main/java/com/food/roots/domain/repository/CozinhaRepository.java
+++ src/main/java/com/food/roots/domain/repository/CozinhaRepository.java
@@ -2,11 +2,16 @@ package com.food.roots.domain.repository;
 
 import com.food.roots.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-    List<Cozinha> nome(String nome);
+    List<Cozinha> findByNomeContaining(String nome);
+
+    @Query("FROM Cozinha WHERE nome like %:cozinhaRestaurante%")
+    List<Cozinha> buscarCozinhasPorNome(@Param("cozinhaRestaurante") String nome);
 }
