cc650e35d4ef1366b0472a5d3493b83126ad33d0
João Geraldo <joaogeraldo40@gmail.com>
2022-12-26 16:39:03 -0300

    Mapeamento de OneToMany para cozinha com Restaurante

--- src/main/java/com/food/roots/domain/model/Cozinha.java
+++ src/main/java/com/food/roots/domain/model/Cozinha.java
@@ -6,11 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -26,5 +30,9 @@ public class Cozinha {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "NOME")
     private String nome;
+
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 }
